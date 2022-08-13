9ad1c1f590f7a2251c182c2e27fd4b7bf122bc6d
yavidor <yonatan@avidor.org>
2022-08-13 11:04:44 +0300

    Small steps

--- main.py
+++ main.py
@@ -2,13 +2,53 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+
 from point3D import Point3D
+import pygame
+
+
+def center_origin(surf, p):
+    return p[0] + surf.get_width() // 2, p[1] + surf.get_height() // 2
 
 
 def setup():
     print("hello")
     point = Point3D(1, 1, 1, 0, 1)
     print(point.neighbors())
+    window_width = 400
+    window_height = 400
+    pygame.init()
+    size = (window_width, window_height)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Game")
+    cube = []
+    offset = -50
+    length = 10
+    cube.append(Point3D(0, 0, 0, offset, length))
+    cube.append(Point3D(0, 0, 1, offset, length))
+    cube.append(Point3D(0, 1, 0, offset, length))
+    cube.append(Point3D(0, 1, 1, offset, length))
+    cube.append(Point3D(1, 0, 0, offset, length))
+    cube.append(Point3D(1, 0, 1, offset, length))
+    cube.append(Point3D(1, 1, 0, offset, length))
+    cube.append(Point3D(1, 1, 1, offset, length))
+    finish = False
+    while not finish:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finish = True
+        draw(cube, screen)
+        pygame.display.flip()
+    pygame.quit()
+
+
+def draw(cube, screen):
+    for i in range(len(cube)):
+        cube[i].rotate(0.1, 'x')
+        pygame.draw.circle(screen, (255,255,255),
+                           center_origin(screen, cube[i].get2dcoords()), 5)
+    # pygame.draw.rect(screen,(255,255,255),)
 
 
 # Press the green button in the gutter to run the script.
--- point3D.py
+++ point3D.py
@@ -1,8 +1,27 @@
+import math
+
+
+def multiply(mat1, mat2):
+    if len(mat1[0]) != len(mat2):
+        return -1
+    m = len(mat1)
+    n = len(mat1[0])
+    p = len(mat2[0])
+    mat3 = [[0] * p] * m
+    for i in range(m):
+        for j in range(p):
+            mat3[i][j] = 0
+            for k in range(n):
+                mat3[i][j] += mat1[i][k] * mat2[k][j]
+    return mat3
+
+
 class Point3D:
     def __init__(self, _x, _y, _z, _offset, _size):
-        self.x = _x
-        self.y = _y
-        self.z = _z
+        self.rotations = None
+        self.x = _x * _size + _offset
+        self.y = _y * _size + _offset
+        self.z = _z * _size + _offset
         self.offset = _offset
         self.size = _size
         self.binary = f'{int((self.x - self.offset) / self.size)}{int((self.y - self.offset) / self.size)}{int((self.z - self.offset) / self.size)}'
@@ -19,3 +38,38 @@ class Point3D:
             print(clone)
             ret.append(int(''.join(clone), 2))
         return list(filter(lambda x: 0 <= x <= 7, ret))
+
+    def get2dcoords(self):
+        newcoords = multiply(
+            [[math.sqrt(3), 0, -math.sqrt(3)], [1, 2, 1], [math.sqrt(2), -math.sqrt(2), math.sqrt(2)]],
+            [[self.x], [self.y], [self.z]])
+        return self.x,self.y
+        return (1 / math.sqrt(6)) * newcoords[0][0], (1 / math.sqrt(6)) * newcoords[1][0]
+
+    def rotate(self, theta, axis):
+        theta = math.radians(theta)
+        self.rotations = {
+            'x': [
+                [1, 0, 0],
+                [0, math.cos(theta), -math.sin(theta)],
+                [0, math.sin(theta), math.cos(theta)],
+            ],
+            'y': [
+                [math.cos(theta), 0, math.sin(theta)],
+                [0, 1, 0],
+                [-math.sin(theta), 0, math.cos(theta)],
+            ],
+            'z': [
+                [math.cos(theta), -math.sin(theta), 0],
+                [math.sin(theta), math.cos(theta), 0],
+                [0, 0, 1],
+            ]
+        }
+        newpoint = multiply(self.rotations[axis], [[self.x], [self.y], [self.z]])
+        print(newpoint)
+        self.x = newpoint[0][0]
+        self.y = newpoint[1][0]
+        self.z = newpoint[2][0]
+        print(self.x)
+        print(self.y)
+        print(self.z)
